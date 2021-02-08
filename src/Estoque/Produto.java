2c9cb38ca0506277efd1c3b0ce21166db6e471fe
Igor Nogueira <igornogueira96@gmail.com>
2021-02-08 20:04:11 -0300

    Criada classe principal e inicialização de janela. Criado esboço do painel inicial.

---
+++ src/Estoque/Produto.java
@@ -0,0 +1,10 @@
+package Estoque;
+
+public class Produto {
+    String rotulo, descricao;
+    float precoCusto, precoVenda;
+    int qtdEstoque;
+    
+    //TODO validar campos com janelas
+    //TODO construtor, getter, setter
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author igorn
+ */
+public class Principal {
+    public static void main(String[] args) {
+        JFrame janela = new JFramePrincipal();
+        janela.setVisible(true);
+    }
+}
