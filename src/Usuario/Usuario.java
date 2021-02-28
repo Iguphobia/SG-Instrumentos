8258e11a4d3d3e20dde52e51af34ead0c64b9946
Iguphobia <igornogueira96@gmail.com>
2021-02-27 22:46:09 -0300

    Criada a classe de usuários, e adicionados atributos. O campo de senha não está criptografado porque o projeto tá meio atrasado. Se sobrar tempo antes da deadline dá pra implementar.

---
+++ src/Usuario/Usuario.java
@@ -0,0 +1,63 @@
+
+package Usuario;
+
+public class Usuario {
+    String Username = "";
+    String Senha = "";
+    String Nome = "";
+    String Telefone = "";
+    String Endereço = "";
+    boolean Admin = false;
+
+    public Usuario(String username, String senha, String nome, String telefone, String endereco, boolean admin) {
+        this.Username = username;
+        this.Senha = senha;
+        this.Nome = nome;
+        this.Telefone = telefone;
+        this.Endereço = endereco;
+        this.Admin = admin;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    public String getEndereço() {
+        return Endereço;
+    }
+
+    public void setEndereço(String Endereço) {
+        this.Endereço = Endereço;
+    }
+
+    public boolean isAdmin() {
+        return Admin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.Admin = isAdmin;
+    }
+    
+    
+    
+}
